f957ceace3382855fcbf123bb66241f0672f249e
Wenlong Jiang <jwlbjtu@gmail.com>
2020-03-13 00:39:03 -0400

    google map

--- src/App.js
+++ src/App.js
@@ -2,23 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import WrappedMap from './components/google-maps/google-map-component';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{width: `100vw`, height: `100vh`}}>
+      <WrappedMap 
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${
+          process.env.REACT_APP_GOOGLE_KEY
+        }`}
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `100%` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+      />
     </div>
   );
 }
---
+++ src/components/google-maps/google-map-component.jsx
@@ -0,0 +1,75 @@
+import React, { useState, useEffect } from 'react';
+import { 
+    GoogleMap, 
+    withScriptjs, 
+    withGoogleMap, 
+    Marker,
+    InfoWindow
+} from 'react-google-maps';
+import * as parkData from '../../data/skateboard-parks.json';
+import mapStyles from './map-styles';
+
+function Map() {
+
+    const [selectedPark, setSelectedPark] = useState(null);
+
+    useEffect(() => {
+        let listener = e => {
+            if(e.key === "Escape") {
+                setSelectedPark(null);
+            }
+        };
+        window.addEventListener("keydown", listener);
+    }, []);
+
+   return(
+    <GoogleMap 
+        defaultZoom={10}
+        defaultCenter={{lat: 45.4211, lng: -75.6903}}
+        defaultOptions={{styles: mapStyles}}
+    >
+        {
+            parkData.features.map(park => (
+                <Marker 
+                    key={park.properties.PARK_ID} 
+                    position={{
+                        lat: park.geometry.coordinates[1],
+                        lng: park.geometry.coordinates[0]
+                    }}
+                    onClick={() => {
+                        setSelectedPark(park);
+                    }}
+                    icon={{
+                        url: "/skateboarding.svg",
+                        scaledSize: new window.google.maps.Size(25, 25)
+                    }}
+                />
+            ))
+        }
+
+        {
+            selectedPark && (
+                <InfoWindow
+                    position={{
+                        lat: selectedPark.geometry.coordinates[1],
+                        lng: selectedPark.geometry.coordinates[0]
+                    }}
+                    onCloseClick={() => {
+                        setSelectedPark(null);
+                    }}
+                >
+                    <div>
+                        <h2>{selectedPark.properties.NAME}</h2>
+                        <p>{selectedPark.properties.DESCRIPTIO}</p>
+                    </div>
+                </InfoWindow>
+            )
+        }
+
+    </GoogleMap>
+    )
+}
+
+const WrappedMap = withScriptjs(withGoogleMap(Map));
+
+export default WrappedMap;
