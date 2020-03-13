387527dc895ea9a8ebf979d2229be0f03c727275
Wenlong Jiang <jwlbjtu@gmail.com>
2020-03-13 00:51:43 -0400

    Updates

---
+++ precache-manifest.b983f9b705f5d0a27d1c07cc75b7fe23.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f6cfea63863da59170fe75e4e31317a",
+    "url": "/Fight_Covid19_NYC/index.html"
+  },
+  {
+    "revision": "6f908199644a2ffc850e",
+    "url": "/Fight_Covid19_NYC/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7e8b0030528457930300",
+    "url": "/Fight_Covid19_NYC/static/js/2.4ff16edf.chunk.js"
+  },
+  {
+    "revision": "577ef60f73636c6e95b316a24bc4f83f",
+    "url": "/Fight_Covid19_NYC/static/js/2.4ff16edf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f908199644a2ffc850e",
+    "url": "/Fight_Covid19_NYC/static/js/main.a80ffc41.chunk.js"
+  },
+  {
+    "revision": "7ab81c3fdd9a21ce1e4f",
+    "url": "/Fight_Covid19_NYC/static/js/runtime-main.b13e81ff.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Fight_Covid19_NYC/static/media/logo.5d5d9eef.svg"
+  }
+]);
